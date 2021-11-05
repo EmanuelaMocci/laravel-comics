1445e158e0d417c26de41eef551cb4386d6bcf6a
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-05 18:05:09 +0100

    Aggiunta header-bottom

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,6 +7,23 @@
     </div>
 
     <div class="header-bottom">
-
+        <img src="{{asset('laravel-comics/images/dc-logo.png')}}" alt="Logo DC Comics">
+        <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP <i class="fas fa-caret-down"></i></a></li>
+        </ul>
+        <div class="search">
+            <input type="search" placeholder="Search">
+            <i class="fas fa-search"></i>
+        </div>
+        
     </div>
 </header>
