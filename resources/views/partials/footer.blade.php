19b7755808b5d3c9081768d69bcf1e511434f3e1
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-06 13:05:27 +0100

    Aggiunta footer-top

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,56 @@
+<footer>
+    <div class="footer-top">
+        <div class="lista-container">
+            <div class="lista">
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+    
+                <h4 class="shop">SHOP</h4>
+                <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+        
+            <div class="lista">
+                <h4>DC</h4>
+                <ul>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+    
+            <div class="lista">
+                <h4>SITES</h4>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>  
+        </div>
+        <div class="cookies">
+            All Site Content TM and © 2020 DC Entertaiment, unless otherwise <span>noted here.</span> All rights reserved. <br>
+            <span>Cookies settings</span>
+        </div>
+    </div>
+</footer>
